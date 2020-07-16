4e08f9fae3c4c4200b7ccddf485f327924388012
ezhiltsov <tommy100@mail.ru>
2020-07-16 10:51:48 +0200

    test(): setup codeception

---
+++ tests/_support/Helper/Acceptance.php
@@ -0,0 +1,25 @@
+<?php
+namespace Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Acceptance extends \Codeception\Module\WebDriver
+{
+	/**
+	 * Wait until all ajax queries will pass on testing page
+	 *
+	 * @param integer $timeout
+	 *
+	 * @return void
+	 */
+	public function waitForAjax($timeout = 30)
+	{
+		$this->waitForJS('return $.active == 0;', $timeout);
+	}
+
+	public function waitForPageLoaded($timeout = 30)
+	{
+		$this->waitForJS('return document.readyState === "complete"', $timeout);
+	}
+}
